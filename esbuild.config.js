6e4d00c268bf335ac1dfe68d38ef0625ef2ebee0
helau64 <heather@hactar.is>
2022-12-16 14:49:08 +0000

    Assets compiling with esbuild

---
+++ esbuild.config.js
@@ -0,0 +1,36 @@
+// https://bilalbudhani.com/chokidar-esbuild/
+
+const esbuild = require('esbuild');
+const chokidar = require('chokidar');
+const postcss = require('esbuild-postcss');
+
+const watchDirectories = [
+    './app/assets/css/**/*.css'
+]
+
+const config = {
+    entryPoints: ['app/assets/css/main.css'],
+    outfile: '_site/static/css/main.css',
+    bundle: true,
+    minify: true,
+    plugins: [
+        postcss(),
+    ],
+    watch: process.argv.includes("--watch"),
+    incremental: process.argv.includes("--watch"),
+    sourcemap: process.argv.includes("--watch"),
+};
+
+if (process.argv.includes("--watch")) {
+
+    (async () => {
+        const result = await esbuild.build(config);
+        chokidar.watch(watchDirectories).on('all', (event, path) => {
+        console.log(`Rebuilding ${path}`)
+        result.rebuild()
+        })
+    })();
+
+} else {
+    esbuild.build(config).catch(() => process.exit(1))
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,14 @@
 module.exports = {
+  syntax: 'postcss-scss',
   plugins: {
-    'postcss-import': {},
+    'postcss-easy-import': {},
+    'postcss-advanced-variables': {},
+    'postcss-atroot': {},
+    'postcss-map-get': {},
+    'postcss-property-lookup': {},
+    'postcss-rem': {baseline: 16},
     'tailwindcss/nesting': {},
     tailwindcss: {},
-    autoprefixer: {},
+    autoprefixer: {}
   },
 };
