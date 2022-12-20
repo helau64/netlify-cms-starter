1ce50e6bcd286fac03d18a5621f84cbc88556301
helau64 <heather@hactar.is>
2022-12-20 09:59:47 +0000

    Add breakpoints to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 module.exports = {
   content: ["./**/*.njk"],
   theme: {
-    container: {
-      center: true,
-    },
-    extend: {
-      colors: {},
-    },
+    screens: {
+      xxs: '350px',
+      xs: '580px',
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px'
+    }
   },
   variants: {},
   plugins: [require("@tailwindcss/typography")],
